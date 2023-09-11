9951b0c333cb9f5cb14e4c5bc6856a65d10af53d
Matteo Gallo <matteo4.gallo@mail.polimi.it>
2023-09-11 22:36:55 +0200

    Verification added

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from typing import Any
 import discord
 from discord.ext import commands
 import slashCommands
-import moderation.views as views
+import moderation.views
 import moderation.moderation as moderation
 import config
 import logging
 import sys
+import verification.views
 
 
 class MyBot(commands.Bot):
@@ -25,6 +26,8 @@ class MyBot(commands.Bot):
         self.warningChannel = None
         self.dmsChannel = None
         self.errorChannel = None
+        self.verifyChannel = None
+        self.checkVerificationChannel = None
 
     async def on_ready(self):
         await self.add_cog(
@@ -34,7 +37,19 @@ class MyBot(commands.Bot):
         self.errorChannel = self.get_channel(config.errorChannelId)
         self.warningChannel = self.get_channel(config.warningsChannelId)
         self.dmsChannel = self.get_channel(config.dmsChannelId)
+        self.verifyChannel = self.get_channel(config.verifyChannelId)
+        self.checkVerificationChannel = self.get_channel(
+            config.checkVerificationChannelId
+        )
         # await self.change_presence(status=discord.Status.online)
+
+        view = verification.views.VerificationView(self)
+        try:
+            self.add_view(view)
+        except:
+            await self.verifyChannel.send(view=view, embed=view.embed)
+            print("create")
+
         print("Mikey is up".format(self.user.name))
 
         self.ready = True
@@ -42,22 +57,55 @@ class MyBot(commands.Bot):
     async def on_message(self, message: discord.Message):
         if message.author == self.user:
             return
-        
+
+        """
         if isinstance(message.channel,discord.DMChannel):
             if message.attachments:
-                print(message.attachments[0].url)
+                if verification.views.isVerifying(message.author.id, checkConnection=True):
+                    await self.checkVerificationChannel.send(message.attachments[0].url)
+                if verification.views.isVerifying(message.author.id, checkSteamId=True):
+                    await self.checkVerificationChannel.send(message.attachments[0].url)
+        """
+        if message.channel.id == self.dmsChannel.id:
+            if message.attachments:
+                if verification.views.isVerifying(
+                    userId=message.author.id, checkConnection=True
+                ):
+                    view = verification.views.VerifyView(
+                        self,
+                        user=message.author,
+                        file=message.attachments[0].url,
+                        connection=True,
+                    )
+                    await self.checkVerificationChannel.send(
+                        view=view, embed=view.embed
+                    )
+                elif verification.views.isVerifying(
+                    userId=message.author.id, checkSteamId=True
+                ):
+                    view = verification.views.VerifyView(
+                        self,
+                        user=message.author,
+                        file=message.attachments[0].url,
+                        steamId=True
+                    )
+                    await self.checkVerificationChannel.send(
+                        view=view, embed=view.embed
+                    )
 
         if len(moderation.findSwearWords(message.content)) > 0:
             await message.add_reaction("<:WarningFlag:1150224008881647657>")
             await self.issueWarning(message)
 
     async def issueWarning(self, message: discord.Message):
-        view = views.ReportMessageView(message, self.user)
+        view = moderation.views.ReportMessageView(message, self.user)
         await self.warningChannel.send(embed=view.embed, view=view)
 
     async def sendWarning(self, data: moderation.WarningData):
-        view = views.AppealView(bot=self, data=data)
-        await self.dmsChannel.send(embed=view.embed, view=view)
+        view = moderation.views.AppealView(bot=self, data=data)
+        await self.dmsChannel.send(
+            content=f"dms of {data.offender.name}", embed=view.embed, view=view
+        )
 
     async def prova(self, id):
         user = await self.fetch_user(id)
@@ -72,7 +120,7 @@ class MyBot(commands.Bot):
         await channel.send(msg)
 
     async def on_error(self, event_method: str) -> None:
-        print(event_method)
+        print(f"Error: {event_method}")
 
 
 def runBot():
--- verification/views.py
+++ verification/views.py
@@ -7,8 +7,9 @@ import logging
 
 class VerificationView(discord.ui.View):
     def __init__(self, bot):
-        super().__init__(custom_id="VerificationView", timeout=None)
+        super().__init__(timeout=None)
         self.bot = bot
+
         self.embed = discord.Embed(
             title="Verification",
             description="Hi fellow driver, wanna join the leagues? You first have to verify your steam account and your internet connection *other instructions*",
@@ -22,11 +23,16 @@ class VerificationView(discord.ui.View):
     async def verifyConnection(
         self, interaction: discord.Interaction, button: discord.ui.Button
     ) -> None:
-        #TODO clear dms
-        embed = discord.Embed(title="Verify", description="Send a screenshot of the results given by this site:\nThe pic should be like something like this:")
-        await self.bot.dmsChannel.send(embed=embed)
+        # TODO clear dms
+        embed = discord.Embed(
+            title="Verify",
+            description="Send a screenshot of the results given by this site:\nThe pic should be like something like this:",
+        )
+        await self.bot.dmsChannel.send(content=f"dms of {interaction.user.name}", embed=embed)
 
         setVerificationStatus(userId=interaction.user.id, connection="on_going")
+
+        await interaction.response.send_message("Verication sent, check your dms", ephemeral=True)
 
     @discord.ui.button(
         label="Verify Steam Account",
@@ -36,19 +42,74 @@ class VerificationView(discord.ui.View):
     async def verifySteam(
         self, interaction: discord.Interaction, button: discord.ui.Button
     ) -> None:
-        embed = discord.Embed(title="Verify", description="Send a screenshot of the results given by this site:\nThe pic should be like something like this:")
-        await self.bot.dmsChannel.send(embed=embed)
+        embed = discord.Embed(
+            title="Verify",
+            description="Send a screenshot of the results given by this site:\nThe pic should be like something like this:",
+        )
+        await self.bot.dmsChannel.send(content=f"dms of {interaction.user.name}", embed=embed)
 
         setVerificationStatus(userId=interaction.user.id, steamId="on_going")
 
+        await interaction.response.send_message("Verication sent, check your dms", ephemeral=True)
 
 
-def setVerificationStatus(userId: int, connection: str = None, steamId: str = None):
+class VerifyView(discord.ui.View):
+    def __init__(self, bot, user: discord.Member, file = str, connection: bool = False, steamId: bool = False):
+        super().__init__(timeout=config.defaultTimeout)
+        self.bot = bot
+        self.user = user
+
+        self.embed = discord.Embed(
+            title="Verification"
+        )
+        self.connection = connection
+        self.steamId = steamId
+
+        if connection:
+            self.embed.description = f"Is {user.name}'s connection verified?"
+        elif steamId:
+            self.embed.description = f"Is {user.name}'s steamId verified?"
+        else:
+            logging.error("VerifyView parameters missing")
+
+        self.embed.set_image(url=file)
+
+    @discord.ui.button(
+        label="Verify",
+        style=discord.ButtonStyle.green,
+    )
+    async def verify(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ) -> None:
+        if self.connection:
+            setVerificationStatus(userId=self.user.id, connection="done")
+            self.embed.description = f"Is {self.user.name}'s connection verified?"
+        elif self.steamId:
+            setVerificationStatus(userId=self.user.id, steamId="done")
+            self.embed.description = f"Is {self.user.name}'s steamId verified?"
+
+        await self.user.add_roles(discord.utils.get(self.user.guild.roles, id=config.connectionRole))
+        await self.user.send("You are now verified! something else like 'join our leagues'")
+
+        await interaction.response.send_message("User Verified", ephemeral=True)
+
+    async def on_timeout(self) -> None:
+        print("todo") #TODO da fare
+
+
+def setVerificationStatus(
+    userId: int, connection: str = None, steamId: str = None
+) -> None:
     history = utils.read(config.historyPath)
     userId = str(userId)
 
-    if userId in history.keys():
-        history[userId] = {"connection": "no", "steamId": "no"}
+    if not (userId in history.keys()):
+        history[userId] = {}
+    
+    if not ("connection" in history[userId].keys()):
+        history[userId]["connection"] = "no"
+    if not ("steamId" in history[userId].keys()):
+        history[userId]["steamId"] = "no"
 
     if connection != None:
         if history[userId]["steamId"] == "on_going" and connection == "on_going":
@@ -57,6 +118,42 @@ def setVerificationStatus(userId: int, connection: str = None, steamId: str = No
     elif steamId != None:
         if history[userId]["connection"] == "on_going" and steamId == "on_going":
             history[userId]["connection"] = "no"
-        history[userId]["steamId"] = connection
+        history[userId]["steamId"] = steamId
     else:
         logging.error("setVerificationStatus() parameters missing")
+
+    utils.write(config.historyPath, history)
+
+
+def isVerifying(
+    userId: int, checkConnection: bool = False, checkSteamId: bool = False
+) -> bool:
+    return getVerificationStatus(userId, checkConnection, checkSteamId) == "on_going"
+
+
+def isVerified(
+    userId: int, checkConnection: bool = False, checkSteamId: bool = False
+) -> bool:
+    return getVerificationStatus(userId, checkConnection, checkSteamId) == "done"
+
+
+def getVerificationStatus(
+    userId: int, checkConnection: bool = False, checkSteamId: bool = False
+) -> str:
+    history = utils.read(config.historyPath)
+    userId = str(userId)
+
+    if not (userId in history.keys()):
+        history[userId] = {"connection": "no", "steamId": "no"}
+
+    if not ("connection" in history[userId].keys()):
+        history[userId]["connection"] = "no"
+    if not ("steamId" in history[userId].keys()):
+        history[userId]["steamId"] = "no"
+
+    if checkConnection:
+        return history[userId]["connection"]
+    elif checkSteamId:
+        return history[userId]["steamId"]
+    else:
+        logging.error("isVerifing() parameters missing")
