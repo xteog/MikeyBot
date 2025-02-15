9e0465eb8149df3387af04fab3583ed232fe6510
xteog <matteo4.gallo@mail.polimi.it>
2025-02-15 01:08:45 +0100

    Bug fix

--- src/AI/Chat.py
+++ src/AI/Chat.py
@@ -28,7 +28,7 @@ class Chat:
             self.history = [self.formatMessage("user", description)]
 
     def sendMessage(self, user: discord.Member, message: str) -> tuple[str, dict]:
-        nick = user.name#UserDAO(self.dbHandler).getNick(user)
+        nick = UserDAO(self.dbHandler).getNick(user)
         message = f"{nick}({user.id}): " + message
         self.history.append(self.formatMessage("user", message))
 
